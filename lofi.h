3b69a077adc24062ec3cff0f52966c1584d3b3f9
Roshan-R <roshanr2001@gmail.com>
2020-07-05 16:55:24 +0530

    Added a forward and backward button. Streams now cycle through eatch other.

--- lofi.h
+++ lofi.h
@@ -20,16 +20,16 @@ class lofi : public QMainWindow
 public:
 
     void setStreams(){
-        this->liveStation[0] = { "Lauft.fm" , "http://stream.laut.fm/lofi"};
-        this->liveStation[1] = {"Planet Lofi","http://198.245.60.88:8080" };
-        this->liveStation[2] = {"Chill Cafe","http://192.99.35.215:5078"};
-        this->liveStation[3] = {"Stream Name","http://192.99.0.170:5038C"};
-        this->liveStation[4] = {"No Copyright Stream","http://78.129.222.70:33297" };
-    QString nurl5 = "Radio Seguidor - http://176.9.43.216:8236";
-    QString nurl6 = "Chillsky - https://lfhh.radioca.st/stream";
-    QString nurl7 = "The Augmented Ear - http://66.228.41.10:8000/http://thirtythree-45.com:8000";
-    QString nurl8 = "Lofi Hip Hop Radio - https://stream.zeno.fm/0r0xa792kwzuv";
-    QString nurl6 = "RauteMusik.FM Study - http://de-hz-fal-stream07.rautemusik.fm/study";
+    this->liveStation[0] = {"Lauft.fm" , "http://stream.laut.fm/lofi"};
+    this->liveStation[1] = {"Planet Lofi","http://198.245.60.88:8080" };
+    this->liveStation[2] = {"Chill Cafe","http://192.99.35.215:5078"};
+    this->liveStation[3] = {"Stream Name","http://192.99.0.170:5038"};
+    this->liveStation[4] = {"No Copyright Stream","http://78.129.222.70:33297" };
+    this->liveStation[5] = {"Radio Seguidor", "http://176.9.43.216:8236"};
+    this->liveStation[6] = {"Chillsky", "https://lfhh.radioca.st/stream"};
+    this->liveStation[7] = {"The Augmented Ear", "http://66.228.41.10:8000/http://thirtythree-45.com:8000"};
+    this->liveStation[8] = {"Lofi Hip Hop Radio", "https://stream.zeno.fm/0r0xa792kwzuv"};
+    this->liveStation[9] = {"RauteMusik.FM Study", "http://de-hz-fal-stream07.rautemusik.fm/study"};
     }
 
     int playStatus(){
@@ -45,11 +45,41 @@ public:
     }
 
     void setAudio(){
-      this->player.setMedia(QUrl(this->url));
+      this->player.setMedia(QUrl(this->liveStation[this->nowPlaying].url));
+      qInfo()<<this->liveStation[this->nowPlaying].name<<" : "<<this->liveStation[this->nowPlaying].url;
     }
 
+    void nextStation(){
+        if ( this->nowPlaying == 9){
+            this->nowPlaying = 1;
+        }
+        else{
+            this->nowPlaying++;
+        }
+        setAudio();
+        if(this->isPlaying){
+            playAudio();
+        }
+    }
+
+    void previousStation(){
+        if ( this->nowPlaying == 0){
+            this->nowPlaying = 9;
+        }
+        else{
+            this->nowPlaying--;
+        }
+        setAudio();
+        if(this->isPlaying){
+            playAudio();
+        }
+    }
     void playAudio() {
       this->player.play();
+    }
+
+    QString getStationName(){
+        return this->liveStation[this->nowPlaying].name;
     }
 
     void changeVolume(int val) {
@@ -71,8 +101,13 @@ private slots:
 
     void on_Button_pressed();
 
+    void on_pushButton_pressed();
+
+    void on_pushButton_2_pressed();
+
 private:
     int isPlaying = 0;
+    int nowPlaying = 1;
     Ui::lofi *ui;
     Station liveStation[10];
     QMediaPlayer player;
